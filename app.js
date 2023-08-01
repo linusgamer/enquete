d687324b7c612cf7ae25f55a95604c85b518e3f4
linusgamer <141163511+linusgamer@users.noreply.github.com>
2023-08-01 15:51:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
-const curiosities = [
+// Variáveis globais
+var flashcardIndex = 0;
+var flashcards = [
     {
-        question: "Beber álcool ajuda a se aquecer no frio",
-        answer: "Mito!",
-        explanation: "- Explicação: O álcool dilata os vasos sanguíneos, o que pode dar uma sensação de calor temporária. No entanto, essa sensação não aumenta a temperatura corporal e pode até levar a uma perda de calor mais rápida."
+        question: "Pergunta 1",
+        answer: "Resposta 1",
+        explanation: "Explicação 1",
+        isTrue: true
     },
     {
-        question: "O álcool é uma substância psicoativa que pode causar dependência",
-        answer: "Verdade!",
-        explanation: "- Fundamento: O álcool é uma substância que afeta o sistema nervoso central e pode levar ao desenvolvimento de dependência física e psicológica."
+        question: "Pergunta 2",
+        answer: "Resposta 2",
+        explanation: "Explicação 2",
+        isTrue: false
     }
 ];
 
+// Função para exibir o flashcard atual
+function showFlashcard() {
+    var flashcardContainer = document.getElementById("curiosity-container");
+    var flashcard = flashcards[flashcardIndex];
 
-let currentIndex = 0;
-
-const curiosityContainer = document.getElementById("curiosity-container");
-const nextButton = document.getElementById("next-button");
-
-function showCuriosity() {
-    const currentCuriosity = curiosities[currentIndex];
-    curiosityContainer.innerHTML = `
-        <div class="flashcard" onclick="showAnswer(this)">
-            <p class="curiosity-question">${currentCuriosity.question}</p>
-            <p class="curiosity-answer">${currentCuriosity.answer}</p>
-            <p class="curiosity-explanation">${currentCuriosity.explanation}</p>
+    flashcardContainer.innerHTML = `
+        <div class="flashcard">
+            <h2>${flashcard.question}</h2>
+            <p class="curiosity-answer">${flashcard.answer}</p>
+            <p class="curiosity-explanation">${flashcard.explanation}</p>
         </div>
     `;
 }
 
-function showAnswer(flashcard) {
-    flashcard.classList.add("show-answer");
+// Função para exibir o popup
+function showPopup(message) {
+    var popup = document.getElementById("popup");
+    var popupContent = document.getElementById("popup-content");
+    var popupMessage = document.getElementById("popup-message");
+
+    popupMessage.textContent = message;
+    popup.style.display = "flex";
+
+    // Fechar o popup ao clicar no botão "Fechar"
+    document.getElementById("popup-close").addEventListener("click", function() {
+        popup.style.display = "none";
+    });
 }
 
-function nextCuriosity() {
-    currentIndex++;
-    if (currentIndex < curiosities.length) {
-        showCuriosity();
+// Função para verificar a resposta do usuário
+function checkAnswer(isTrue) {
+    var flashcard = flashcards[flashcardIndex];
+
+    if (flashcard.isTrue === isTrue) {
+        showPopup("Você acertou!");
     } else {
-        currentIndex = 0;
-        showCuriosity();
+        showPopup("Você errou!");
+    }
+
+    // Exibir a resposta e explicação
+    var flashcardElement = document.querySelector(".flashcard");
+    flashcardElement.classList.add("show-answer");
+}
+
+// Função para avançar para o próximo flashcard
+function nextFlashcard() {
+    var flashcardContainer = document.getElementById("curiosity-container");
+
+    // Remover a classe "show-answer" do flashcard atual
+    var flashcardElement = document.querySelector(".flashcard");
+    if (flashcardElement) {
+        flashcardElement.classList.remove("show-answer");
+    }
+
+    // Verificar se há mais flashcards
+    if (flashcardIndex < flashcards.length - 1) {
+        flashcardIndex++;
+        showFlashcard();
+    } else {
+        flashcardContainer.innerHTML = "<p>Fim dos flashcards!</p>";
     }
 }
 
-nextButton.addEventListener("click", nextCuriosity);
+// Event listeners para os botões "Verdade" e "Mito"
+document.getElementById("true-button").addEventListener("click", function() {
+    checkAnswer(true);
+});
 
-showCuriosity();
+document.getElementById("false-button").addEventListener("click", function() {
+    checkAnswer(false);
+});
+
+// Exibir o primeiro flashcard ao carregar a página
+showFlashcard();
