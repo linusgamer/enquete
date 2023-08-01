363a3f6b85bfd1424e8e023fda2df82015a7c216
linusgamer <141163511+linusgamer@users.noreply.github.com>
2023-08-01 15:59:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,14 @@
 var flashcardIndex = 0;
 var flashcards = [
     {
-        question: "Pergunta 1",
-        answer: "Resposta 1",
-        explanation: "Explicação 1",
-        isTrue: true
+        question: "Beber álcool ajuda a se aquecer no frio",
+        answer: "Mito!",
+        explanation: "- Explicação: O álcool dilata os vasos sanguíneos, o que pode dar uma sensação de calor temporária. No entanto, essa sensação não aumenta a temperatura corporal e pode até levar a uma perda de calor mais rápida."
     },
     {
-        question: "Pergunta 2",
-        answer: "Resposta 2",
-        explanation: "Explicação 2",
-        isTrue: false
+        question: "O álcool é uma substância psicoativa que pode causar dependência",
+        answer: "Verdade!",
+        explanation: "- Fundamento: O álcool é uma substância que afeta o sistema nervoso central e pode levar ao desenvolvimento de dependência física e psicológica."
     }
 ];
 
@@ -23,59 +21,49 @@ function showFlashcard() {
     flashcardContainer.innerHTML = `
         <div class="flashcard">
             <h2>${flashcard.question}</h2>
-            <p class="curiosity-answer">${flashcard.answer}</p>
-            <p class="curiosity-explanation">${flashcard.explanation}</p>
         </div>
     `;
-}
-
-// Função para exibir o popup
-function showPopup(message) {
-    var popup = document.getElementById("popup");
-    var popupContent = document.getElementById("popup-content");
-    var popupMessage = document.getElementById("popup-message");
-
-    popupMessage.textContent = message;
-    popup.style.display = "flex";
-
-    // Fechar o popup ao clicar no botão "Fechar"
-    document.getElementById("popup-close").addEventListener("click", function() {
-        popup.style.display = "none";
-    });
 }
 
 // Função para verificar a resposta do usuário
 function checkAnswer(isTrue) {
     var flashcard = flashcards[flashcardIndex];
+    var flashcardElement = document.querySelector(".flashcard");
 
     if (flashcard.isTrue === isTrue) {
-        showPopup("Você acertou!");
+        flashcardElement.style.backgroundColor = "#2ecc71";
+        flashcardElement.style.color = "#ffffff";
     } else {
-        showPopup("Você errou!");
+        flashcardElement.style.backgroundColor = "#e74c3c";
+        flashcardElement.style.color = "#ffffff";
     }
 
     // Exibir a resposta e explicação
-    var flashcardElement = document.querySelector(".flashcard");
-    flashcardElement.classList.add("show-answer");
+    flashcardElement.innerHTML += `
+        <p class="curiosity-answer">${flashcard.answer}</p>
+        <p class="curiosity-explanation">${flashcard.explanation}</p>
+    `;
+
+    // Exibir o botão "Próxima Curiosidade"
+    var nextButton = document.getElementById("next-button");
+    nextButton.style.display = "block";
 }
 
 // Função para avançar para o próximo flashcard
 function nextFlashcard() {
     var flashcardContainer = document.getElementById("curiosity-container");
 
-    // Remover a classe "show-answer" do flashcard atual
-    var flashcardElement = document.querySelector(".flashcard");
-    if (flashcardElement) {
-        flashcardElement.classList.remove("show-answer");
-    }
-
     // Verificar se há mais flashcards
     if (flashcardIndex < flashcards.length - 1) {
         flashcardIndex++;
         showFlashcard();
     } else {
-        flashcardContainer.innerHTML = "<p>Fim dos flashcards!</p>";
+        flashcardContainer.innerHTML = "<p>Fim das curiosidades!</p>";
     }
+
+    // Ocultar o botão "Próxima Curiosidade"
+    var nextButton = document.getElementById("next-button");
+    nextButton.style.display = "none";
 }
 
 // Event listeners para os botões "Verdade" e "Mito"
@@ -86,6 +74,9 @@ document.getElementById("true-button").addEventListener("click", function() {
 document.getElementById("false-button").addEventListener("click", function() {
     checkAnswer(false);
 });
+
+// Event listener para o botão "Próxima Curiosidade"
+document.getElementById("next-button").addEventListener("click", nextFlashcard);
 
 // Exibir o primeiro flashcard ao carregar a página
 showFlashcard();
