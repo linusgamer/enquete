c37a6fb23e86edb1aec058d853229673f208e07b
linusgamer <141163511+linusgamer@users.noreply.github.com>
2023-08-01 17:44:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,31 @@ var flashcards = [
         answer: "Mito!",
         explanation: "O café e o banho frio não têm a capacidade de diminuir o efeito do álcool no organismo. A única forma de diminuir o efeito do álcool é esperar o corpo metabolizá-lo naturalmente."
     },
+   {
+        question: "O álcool pode afetar negativamente o desempenho acadêmico e profissional.",
+        answer: "Verdade!",
+        explanation: "O consumo excessivo de álcool pode levar a problemas de memória, dificuldade de concentração e diminuição do desempenho cognitivo, afetando negativamente o desempenho acadêmico e profissional."
+    },
+
+	{
+        question: " Beber álcool antes de dormir ajuda a ter uma boa noite de sono.",
+        answer: "Mito!",
+        explanation: " Embora o álcool possa inicialmente induzir o sono, elepode interromper os ciclos de sono normais e levar a uma qualidadede sono inferior. O consumo de álcool antes de dormir pode resultarem sono fragmentado e menos reparador."
+    },
+
+     {
+        question: " O consumo moderado de álcool pode ter alguns benefícios à saúde, como redução do risco de doenças cardíacas.",
+        answer: "Verdade!",
+        explanation: "Estudos mostram que o consumo moderado de álcool, especialmente vinho tinto, pode estar associado a uma redução do risco de doenças cardíacas. No entanto, é importante ressaltar que os benefícios são observados apenas em quantidades moderadas e para certos grupos populacionais."
+
+    },
+
+	{
+        question: "Beber álcool em excesso pode matar as células cerebrais.",
+        answer: "Mito!",
+        explanation: "O álcool prejudica a coordenação motora, o tempo de reação e a capacidade de julgamento, o que torna a condução sob efeito de álcool extremamente perigosa. Beber álcool antes de dirigir aumenta significativamente o risco de acidentes de trânsito."
+
+    },
 ];
 
 
